df047d1feced08755b8efaef02b653756ea1a71f
Joseph Macri <jmacri@acanpf.com>
2017-08-19 15:18:04 -0400

    updatre

--- Prepare-Merge.ps1
+++ Prepare-Merge.ps1
@@ -10,7 +10,6 @@
 
 $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 . $scriptDir\lib\lib-GitUtils.ps1
-. $scriptDir\lib\lib-DotnetUtils.ps1
 
 $repoLocation = $repoLocation.TrimEnd('\')
 
@@ -31,12 +30,3 @@ $resolvedChildBranchName = Find-GitBranch $childBranchName
 git merge --no-ff --no-commit $resolvedChildBranchName
 
 $pwd = pwd
-
-$assemblyVersion = Get-GlobalAssemblyVersion "${repoLocation}\dotnet\GlobalAssemblyInfo.cs"
-
-$scriptsLocation = "${repoLocation}\devops\powershell"
-cd $scriptsLocation
- 
-.\Rebase-PlaceholderScripts -s r -newVersion $assemblyVersion
-
-cd $pwd
