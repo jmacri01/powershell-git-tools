875ea561feb987187e29dee5b1c8f6f9c22bd382
Joseph Macri <jmacri01@gmail.com>
2022-12-27 19:57:19 -0600

    split video in several locations using ffmpeg

---
+++ Split-Video.ps1
@@ -0,0 +1,27 @@
+﻿param
+(
+    [Parameter(Mandatory=$true)]
+    [String[]] $SplitTimes,
+    [Parameter(Mandatory=$true)]
+    [String] $InputFile,
+    [Parameter(Mandatory=$true)]
+    [String] $OutputFolder,
+    [Parameter(Mandatory=$true)]
+    [String] $OutputFileName,
+    [Parameter(Mandatory=$true)]
+    [String] $OutputFileExt,
+    [int] $FileNoStart = 1,
+    [String]$StartTime = '00:00:00'
+)
+
+foreach($time in $SplitTimes)
+{
+    Write-Host "Start time:: $($StartTime)"
+    Write-Host "End time:: $($time)"
+    Write-Host "File No: $($fileNoStart)"
+
+    ffmpeg -i $InputFile -vcodec copy -acodec copy -ss $StartTime -to $time "$($OutputFolder)episode$($fileNoStart).$($OutputFileExt)"
+
+    $FileNoStart = $FileNoStart + 1
+    $StartTime = $time
+}
