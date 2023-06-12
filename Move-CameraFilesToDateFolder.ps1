7e840d518b00089de4ce0556acc3b16ae48bdbd6
Joseph Macri <jmacri01@gmail.com>
2023-06-12 09:12:50 -0500

    camera tools

---
+++ Move-CameraFilesToDateFolder.ps1
@@ -0,0 +1,34 @@
+﻿param
+(
+    [string]$directory
+)
+
+if (!($directory))
+{
+    throw "$directory can not be null"
+}
+
+$colItems = @(Get-ChildItem $directory -Include $Extension | sort-object -property LastWriteTime | Where-Object { $_.Mode -ne 'd-----' })
+
+foreach($colItem in $colItems)
+{
+    if(-not ($colItem.Name -match "_\d\d\d\d\-\d\d\-\d\d_"))
+    {
+        continue
+    }
+
+    $date = ($colItem.Name -replace ".*(?=\d\d\d\d\-\d\d\-\d\d)", "") -replace "(?<=\d\d\d\d\-\d\d\-\d\d).*", ""
+    if($date -match "\d\d\d\d\-\d\d\-\d\d")
+    {
+        $directory = $colItem.DirectoryName
+        $fileName = $colItem.Name
+
+        if(-not (Test-Path "$directory\$date"))
+        {
+            mkdir "$directory\$date"
+        }
+
+        Move-Item -Path $colItem.FullName -Destination "$directory\$date\$fileName"
+        Write-Host "Moving $fileName to $directory\$date\$fileName"
+    }
+}
---
+++ Truncate-Directory.ps1
@@ -0,0 +1,37 @@
+﻿param
+(
+    [string]$directory,
+    [decimal]$sizeLimitGb
+)
+
+if (!($directory))
+{
+    throw "$directory can not be null"
+}
+
+while(1)
+{
+    $dirSize = 0
+
+    $colItems = @(Get-ChildItem $directory -Include $Extension -Recurse | sort-object -property LastWriteTime | Where-Object { $_.Mode -ne 'd-----' })
+
+    foreach ($i in $colItems)
+    {
+        $dirSize = $dirSize + $i.Length / 1GB
+    }
+
+    if(($dirSize -lt $sizeLimitGb) -or !($colItems[0]))
+    {
+        return
+    }
+
+    remove-item $colItems[0].FullName
+    $colItems[0].FullName + " removed!"
+
+    $dirItems = @(Get-ChildItem $colItems[0].DirectoryName -Recurse | sort-object -property LastWriteTime | Where-Object { $_.Mode -ne 'd-----' })
+    if($dirItems.Count -eq 0)
+    {
+        Remove-Item $colItems[0].DirectoryName -Recurse -Force
+        $colItems[0].DirectoryName + " removed!"
+    }
+}
